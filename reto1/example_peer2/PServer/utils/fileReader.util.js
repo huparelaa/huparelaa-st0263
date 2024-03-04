f3452cc9c95f38d10f856f0acc30f86dabd1487e
Hobar <huparelaa@eafit.edu.co>
2024-03-04 11:51:44 -0500

    🚚 Move config.json to Peer level

--- reto1/example_peer2/PServer/utils/fileReader.util.js
+++ reto1/example_peer2/PServer/utils/fileReader.util.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url';
 function readConfigFile() {
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = path.dirname(__filename);
-    const file_path = path.join(__dirname, '../config.json');
+    const file_path = path.join(__dirname, '../../config.json');
 
     try {
         const data = fs.readFileSync(file_path, 'utf8');
